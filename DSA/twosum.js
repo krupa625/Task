f4fa7fcb1e00e069e4bb1b152094ff5ebbafc5f9
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-07 15:36:52 +0530

    Update twosum.js

--- DSA/twosum.js
+++ DSA/twosum.js
@@ -20,7 +20,4 @@ var twoSum = function(nums, target) {
     throw new Error("No solution found");
 };
 
-// Example usage:
-console.log(twoSum([2, 7, 11, 15], 9));  // Output: [0, 1]
-console.log(twoSum([3, 2, 4], 6));       // Output: [1, 2]
-console.log(twoSum([3, 3], 6));          // Output: [0, 1]
+
