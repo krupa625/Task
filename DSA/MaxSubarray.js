49cf3b0e6b88fa5c4b243b94d7816c15fd8b78a8
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-08 10:27:55 +0530

    Update MaxSubarray.js

--- DSA/MaxSubarray.js
+++ DSA/MaxSubarray.js
@@ -1,26 +1,26 @@
 function maxSubArray(nums) {
-    // Step 1: Initialize the maximum subarray sum to the first element
+    // intialization of max array
     let max = nums[0];
     
-    // Step 2: Initialize the sum of the present subarray to 0
+    // current array sum intit with 0
     let sum = 0;
 
-    // Step 3: Iterate  array through
+    // loop for array 
     for (let i = 0; i < nums.length; i++) {
         // Add the current element to the running sum
         sum += nums[i];
 
-        // Step 4: Update max if the current sum is greater than the previous max
+        // replace max if sum is greater
         if (sum >= max) {
             max = sum;
         }
 
-        // Step 5: If the running sum becomes negative, reset it to 0
+        // if nagative replace with 0
         if (sum <= 0) {
             sum = 0;
         }
     }
 
-    // Step 6: Return the maximum sum found
+    // retuen the sum which is found
     return max;
 }
