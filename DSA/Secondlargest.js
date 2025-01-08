d1e8471096187a9a409197b39e298df39c91bcbc
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-08 17:28:15 +0530

    Update Secondlargest.js

--- DSA/Secondlargest.js
+++ DSA/Secondlargest.js
@@ -1,24 +1,24 @@
 class Solution {
     // Function returns the second largest element
     getSecondLargest(arr) {
-        // Step 1: Initialize two variables to keep track of largest and second largest numbers
+        //  Initialize two variables to keep track of largest and second largest numbers
         let large = -1;    
         let slarge = -1;   
 
-        // Step 2: Loop through the array to find the largest and second largest elements
+        // Loop through the array to find the largest and second largest elements
         for (let i = 0; i < arr.length; i++) {
-            // Step 3: If the current element is greater than 'large', update both 'large' and 'slarge'
+            //  If the current element is greater than 'large', update both 'large' and 'slarge'
             if (arr[i] > large) {
                 slarge = large;  
                 large = arr[i];   // Update 'large' to the current element
             }
-            // Step 4: If the current element is greater than 'slarge' and not equal to 'large', update 'slarge'
+            //  If the current element is greater than 'slarge' and not equal to 'large', update 'slarge'
             else if (arr[i] > slarge && arr[i] !== large) {
                 slarge = arr[i];  
             }
         }
 
-        // Step 5: Return the second largest number found
+        //  Return the second largest number found
         return slarge;  // If no second largest found, it will return -1
     }
 }
