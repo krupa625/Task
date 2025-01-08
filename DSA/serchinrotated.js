ad114093995d0d6ff9adad901b6c04facba68846
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-08 17:31:08 +0530

    Update serchinrotated.js

--- DSA/serchinrotated.js
+++ DSA/serchinrotated.js
@@ -5,19 +5,19 @@
  * @returns {number} - The index of the target value, or -1 if not found.
  */
 function search(nums, target) {
-    // Initialize the low and high pointers
+  
     let low = 0, high = nums.length - 1;
 
-    // Perform binary search
+
     while (low <= high) {
-        // Calculate the mid index
+       
         let mid = Math.floor((high + low) / 2);
 
-        // If the target is found at mid, return the index
+       
         if (nums[mid] === target) {
             return mid;
         }
-        // If the left half is sorted
+        
         else if (nums[mid] >= nums[low]) {
             
             if (nums[low] <= target && target <= nums[mid]) {
@@ -28,7 +28,7 @@ function search(nums, target) {
         }
         
         else {
-            // Check if the target lies within the right half
+            
             if (nums[mid] <= target && target <= nums[high]) {
                 low = mid + 1;
             } else {
