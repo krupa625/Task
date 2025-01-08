f411a340adb2d40bbddfbede9bd7138b57981d40
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-08 09:37:52 +0530

    Update sumofN.js

--- DSA/sumofN.js
+++ DSA/sumofN.js
@@ -2,12 +2,12 @@ var sumZero = function(n) {
     let result = [];
     
     for (let k = 1; k <= Math.floor(n / 2); k++) {
-        result.push(k, -k);
+        result.push(k, -k); // add nagative and positive value of k 
     }
     
     if (n % 2 !== 0) {
-        result.push(0);
+        result.push(0);// if k is odd then return 0 
     }
     
-    return result;
+    return result;//result of sum 
 };
