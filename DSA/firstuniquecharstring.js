08e648dac23b57f7f7a49138ceba7133b2f83e7d
Krupa Patel <142816579+krupa625@users.noreply.github.com>
2025-01-09 15:49:51 +0530

    Update firstuniquecharstring.js

--- DSA/firstuniquecharstring.js
+++ DSA/firstuniquecharstring.js
@@ -1,22 +1,8 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var firstUniqChar = function(s) {
-    let mp = {};
-
-    for (let a of s) {
-        mp[a] = (mp[a] || 0) + 1;
-    }
-
     for (let i = 0; i < s.length; i++) {
-        if (mp[s[i]] === 1) {
+        if (s.indexOf(s[i]) === i && s.indexOf(s[i], i + 1) === -1) {
             return i;
         }
     }
-
     return -1;
 };
-
-
-//time complexity : O(n)
